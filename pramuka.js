ac5277e1baeab314b8604b7d64abef84c0d78f18
irawanria85 <irawanria85@gmail.com>
2020-11-12 05:16:37 +0700

    Update and rename myblog.js to pramuka.js

---
+++ pramuka.js
@@ -0,0 +1,28 @@
+var pages = [ 
+
+"https://pejuangpramuka.blogspot.com/p/beautiful-girl.html",
+"https://pejuangpramuka.blogspot.com/p/beautiful-girls.html",
+"https://pejuangpramuka.blogspot.com/p/hot-news.html",
+"https://pejuangpramuka.blogspot.com/p/pretty-girl.html",
+"https://pejuangpramuka.blogspot.com/p/pretty-girls.html",
+"https://pejuangpramuka.blogspot.com/p/sweety-girl.html",
+"https://pejuangpramuka.blogspot.com/p/sweety-girls.html",
+"https://pejuangpramuka.blogspot.com/p/tik-tok-star.html",
+"https://pejuangpramuka.blogspot.com/p/top-trending.html",
+"https://pejuangpramuka.blogspot.com/p/trending-topic.html",
+"https://pejuangpramuka.blogspot.com/p/viral-news.html",
+"https://pejuangpramuka.blogspot.com/p/virgin-girl.html",
+"https://spewehascouter.blogspot.com/p/asian-girls.html",
+"https://spewehascouter.blogspot.com/p/beautiful-girls.html",
+"https://spewehascouter.blogspot.com/p/best-potrait-girls.html",
+"https://spewehascouter.blogspot.com/p/happy-girls.html",
+"https://spewehascouter.blogspot.com/p/pretty-girls.html",
+"https://spewehascouter.blogspot.com/p/sweety-girls.html",
+"https://spewehascouter.blogspot.com/p/top-of-girls-gallery.html",
+"https://spewehascouter.blogspot.com/p/wallpaper.html",
+
+];
+function randomPage() { 
+return pages[Math.round(Math.random() * (pages.length - 1))]; 
+} 
+location.href= randomPage(); 
