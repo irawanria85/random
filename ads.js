641d42be16a19ee6a662c2cd629557335014c3d2
irawanria85 <irawanria85@gmail.com>
2020-12-05 10:02:57 +0700

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,36 @@
+var pages = [ 
+
+"https://jandacantikkaya.blogspot.com/",
+"https://keepopedia.blogspot.com/",
+"https://setahunbaru.blogspot.com/",
+"https://pintergram.blogspot.com/",
+"https://carijandaonline.blogspot.com/",
+"https://jutaanjanda.blogspot.com/",
+"https://beritanasional6.blogspot.com/",
+"https://indonesiwa.blogspot.com/",
+"https://dagelanpedia.blogspot.com/",
+"https://spewehascouter.blogspot.com/",
+"https://bikinmasakan.blogspot.com/",
+"https://beritanesiya.blogspot.com/",
+"https://sejutashare.blogspot.com/",
+"https://pinogram.blogspot.com/",
+"https://sejutakuota.blogspot.com/",
+"https://cuperhero.blogspot.com/",
+"https://surgajanda.blogspot.com/",
+"https://pinmotivasi.blogspot.com/",
+"https://jandamudaseksi.blogspot.com/",
+"https://viralindongsay.blogspot.com/",
+"https://piniterest.blogspot.com/",
+"https://pejuangpramuka.blogspot.com/",
+"https://sejutakita.blogspot.com/",
+"https://bayarpedia.blogspot.com/",
+"https://hasdukmerahputih.blogspot.com/",
+"https://kuotagram.blogspot.com/",
+"https://www.indonesiya.com/",
+"https://www.hasdukmerahputih.com/",
+
+];
+function randomPage() { 
+return pages[Math.round(Math.random() * (pages.length - 1))]; 
+} 
+location.href= randomPage(); 
